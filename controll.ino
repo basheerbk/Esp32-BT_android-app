02cdf72840bbcffe1a09c0e29f8dce8d13083f2b
Muhammad Basheer <49371247+basheerbk@users.noreply.github.com>
2022-08-07 00:07:54 +0530

    Add files via upload

---
+++ controll.ino
@@ -0,0 +1,35 @@
+
+#include"BluetoothSerial.h"  // this header is needed for Bluetooth Serial
+
+BluetoothSerial ESP_BT; 
+
+int Relay = 13;
+
+    
+int incoming;  // Parameters for Bluetooth interface
+
+void setup() {
+  Serial.begin(19200);
+  ESP_BT.begin("ESP32_EC"); //Name of your Bluetooth interface -> will show up on your phone
+  pinMode (Relay, OUTPUT);
+  
+}
+
+void loop() {
+  //Receive Bluetooth signal
+  
+  if (ESP_BT.available()) 
+  {
+    incoming = ESP_BT.read(); 
+    
+    int button = floor(incoming / 10); //button ID is (10,11),value is 1 or 0 You Add More Button (20,21),(30,31)........
+    int value = incoming % 10;
+    
+    switch (button) {
+      case 1:  
+        Serial.print("Button 1:"); Serial.println(value);
+        digitalWrite(Relay, value);
+        break;
+    }
+  }
+}
